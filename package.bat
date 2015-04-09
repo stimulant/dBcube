e48ad1bccc6e7a015a0b3bbe45993ae14ebbeff2
Josh Santangelo <josh@stimulant.com>
2015-04-08 17:29:42 -0700

    Create package.bat

---
+++ package.bat
@@ -0,0 +1,22 @@
+REM DIE IF NOT ON BUILD SERVER
+IF %COMPUTERNAME% NEQ JENKINS GOTO NOTBUILD
+
+REM SET UP ENVIRONMENT
+SET DIRSWITCHES=/v /np /njs /njh /bytes /fft /ndl /mir
+SET FILESWITCHES=/v /np /njs /njh /bytes /fft /ndl 
+
+SET TARGET=..\builds\%BUILD_ID%\archive\
+ECHO %BUILD_NUMBER% > %TARGET%\BUILD_%BUILD_NUMBER%
+
+ROBOCOPY Client\vc2012\Release\ %TARGET%\Client %DIRSWITCHES%
+ROBOCOPY Server %TARGET%\Server %DIRSWITCHES%
+
+del %TARGET%\Client\*.obj /s /q
+del %TARGET%\Client\*.tlog /s /q
+del %TARGET%\Client\*.pdb /s /q
+
+EXIT /B 0
+
+:NOTBUILD
+ECHO This should only be run on the build server.
+PAUSE
